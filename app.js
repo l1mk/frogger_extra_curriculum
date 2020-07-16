d77f52b70d9a91c99cba4df6858cb2130c49b5a6
github username <github email address>
2020-07-15 23:21:25 -0500

    took the down movement

--- app.js
+++ app.js
@@ -33,9 +33,9 @@ function moveFrog(e){
         case 39:
             if (currentIndex % width < width - 1) currentIndex +=1
             break
-        case 40:
-            if (currentIndex % width < width * width) currentIndex += width
-            break
+        //case 40:
+        //    if (currentIndex % width < width * width) currentIndex += width
+        //    break
     }
     squares[currentIndex].classList.add('frog')
     lose()
